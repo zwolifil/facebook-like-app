851bc43e7d4430014db0649fdab2f41d11294373
Filip <zwolifil@outlook.com>
2018-05-19 20:34:47 +0200

    feat(): create sample project

---
+++ src/LoginComponent/Login.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './../logo.svg';
+import './Login.css';
+
+class Login extends Component {
+    render() {
+        return (
+            <div >
+                <header className="nav">
+                    <img src={logo} className="logo" alt="logo" />
+                    <h1 >Welcome to React</h1>
+                </header>
+                <p>
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Login;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Login from './LoginComponent/Login';
+
+ReactDOM.render(<Login />, document.getElementById('root'));
